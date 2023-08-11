5f7f3aea2d4d4de79a5cc77a9b78e832ad592514
ANAMI_R <r.anami@nhkspg.co.jp>
2023-08-11 13:48:54 +0900

    put 2 buttons

--- CountDown.py
+++ CountDown.py
@@ -7,6 +7,8 @@ class Countdown(ft.UserControl):
     def __init__(self, seconds):
         super().__init__()
         self.seconds = seconds
+        self.flg = True
+        self.timeValue = 0
 
     def did_mount(self):
         self.running = True
@@ -23,12 +25,50 @@ class Countdown(ft.UserControl):
             self.update()
             time.sleep(1)
             self.seconds -= 1
+        self.countdown.value="00:00"
+        self.update()
+        self.page.dialog = ft.AlertDialog(modal=True, title=ft.Text("hello"))
+
+        dlg = ft.AlertDialog(title=ft.Text("Take a rest."), on_dismiss=lambda e: print("Dialog dismissed!"))
+        self.page.dialog = dlg
+        dlg.open = True
+        self.page.window_always_on_top = True
+        self.page.update()
 
     def build(self):
-        self.countdown = ft.Text()
-        return self.countdown
+        def switch(e):
+            if self.flg == True:
+                self.flg = False
+            else:
+                self.flg = True
+
+        def reset(e):
+            self.timeValue = 100
+            self.seconds = self.timeValue
+            self.flg = True
+            if self.seconds == 0:                
+                self.th = threading.Thread(target=self.update_timer, args=(), daemon=True)
+                self.th.start()
+
+        self.countdown = ft.Text(size=100)
+        self.reset = ft.ElevatedButton("Reset",on_click=reset)
+        self.pause = ft.ElevatedButton("Pause",on_click=switch)
+        return ft.Column([
+            ft.Row([
+                self.countdown
+            ],alignment=ft.MainAxisAlignment.CENTER),
+            ft.Row([
+                self.reset,
+                self.pause
+            ],alignment=ft.MainAxisAlignment.CENTER)
+        ])
 
 def main(page: ft.Page):
-    page.add(Countdown(120))
+    page.title="タイマーだよ～ん"
+    page.window_resizable=False
+    page.window_width=400
+    page.window_height=300
+    page.vertical_alignment=ft.MainAxisAlignment.CENTER
+    page.add(Countdown(3))
 
 ft.app(target=main)
